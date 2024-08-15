e3c3fe1cb38a448e097f47a586eb8a316a6cd154
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-08-15 13:05:13 +0530

    Add: QuickAccess Route

--- controllers/Root.controller.js
+++ controllers/Root.controller.js
@@ -7,7 +7,30 @@ const __dirname = path.dirname(__filename);
 const RootControllers = {
     getRoot: (req, res) => {
         res.sendFile(path.join(__dirname, '../index.html'));
-    }
+    },
+
+    getQuickAccess: (req, res) => {
+        const constantsData = [
+            'FG Inventory Management',
+            'RM Inventory Management',
+            'Generic Masters',
+            'Migrations',
+            'Formula Procedures',
+            'Point Of Sale',
+            'Production',
+            'Order Management',
+            'Invoicing',
+            'Sub Contracting',
+            'CRM',
+            'Procurement',
+            'Scheme',
+            'Loyalty',
+            'Item Masters',
+            'VRM'
+        ];
+        
+        res.json(constantsData);
+    },
 };
 
 export default RootControllers;
--- routes/Root.router.js
+++ routes/Root.router.js
@@ -4,5 +4,6 @@ import RootControllers from '../controllers/Root.controller.js';
 const RootRouter = express.Router();
 
 RootRouter.get('/' , RootControllers.getRoot)
+RootRouter.get('/QuickAccess' , RootControllers.getQuickAccess)
 
 export default RootRouter;
