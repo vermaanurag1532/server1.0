d39b4617a4ee45ec7397c72f3f5b2873261f24bd
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-08-15 16:34:53 +0530

    Add: QuickAccess Route

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,30 @@
-import mysql from 'mysql2';
+function handleDisconnect() {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Anurag@1532',
+        database: 'mydatabase'
+    });
 
-const connection = mysql.createConnection({
-    host: '13.239.113.142',  
-    user: 'root',    
-    password: 'Anurag@1532',
-    database: 'mydatabase'  
-});
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err.stack);
+            setTimeout(handleDisconnect, 2000); // Retry after 2 seconds
+        } else {
+            console.log('Connected to MySQL as id', connection.threadId);
+        }
+    });
 
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL as id', connection.threadId);
-});
+    connection.on('error', err => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect(); // Reconnect on lost connection
+        } else {
+            throw err;
+        }
+    });
 
+    return connection;
+}
+
+const connection = handleDisconnect();
 export default connection;
