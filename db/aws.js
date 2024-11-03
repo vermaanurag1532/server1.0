9d0d93f612d5f12b501907c5c5f691f1d5c1af8e
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-11-03 15:04:21 +0530

    Add: Routes

--- db/aws.js
+++ db/aws.js
@@ -1,9 +1,9 @@
-// // config.js
-// export default {
-//     s3: {
-//         bucketName: 'jwel',
-//         region: 'ap-south-1',
-//         accessKeyId: 'AKIAS252V4C4OF7NQ5HA',
-//         secretAccessKey: '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'
-//     }
-// };
+// config.js
+export default {
+    s3: {
+        bucketName: 'jwel',
+        region: 'ap-south-1',
+        accessKeyId: '',
+        secretAccessKey: ''
+    }
+};
