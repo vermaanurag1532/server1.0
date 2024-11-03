d83a9f59642efc91d9e79389929ca1cb8c3ae64f
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-11-03 15:08:26 +0530

    Add: Routes

--- db/aws.js
+++ db/aws.js
@@ -1,6 +1,10 @@
-// config.js
-export default {
+const config = {
     s3: {
-
+        region: process.env.AWS_REGION,
+        bucketName: process.env.AWS_BUCKET_NAME,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 };
+
+export default config;
