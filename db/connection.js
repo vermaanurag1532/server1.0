1ca621f4885fd8d87cc4ad4289eef11591377a04
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-11-08 08:08:04 +0530

    ADD: Routes

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,29 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
 
-async function createConnection() {
-    try {
-        const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'Anurag@1532',
-            database: 'mydatabase'
-        });
-        console.log('Connected to MySQL');
-        return connection;
-    } catch (error) {
-        console.error('Error connecting to MySQL:', error);
-        setTimeout(createConnection, 2000); // Reattempt connection if it fails
-    }
+function handleDisconnect() {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        // host: 'localhost',
+        user: 'root',
+        password: 'Anurag@1532',
+        database: 'mydatabase'
+    });
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err.stack);
+            setTimeout(handleDisconnect, 2000); 
+        } else {
+            console.log('Connected to MySQL as id', connection.threadId);
+        }
+    });
+    connection.on('error', err => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+    return connection;
 }
-
-const connection = await createConnection();
+const connection = handleDisconnect();
 export default connection;
