b9046af42d55683130afde1076c8f134a391fab3
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2025-01-04 20:48:37 +0530

    Add routes

--- Repository/Procurement/GoodReceiptNote/GoodReceiptNote.repository.js
+++ Repository/Procurement/GoodReceiptNote/GoodReceiptNote.repository.js
@@ -23,29 +23,35 @@ const ProcurementGoodReceiptRepository = {
         const stockId = await ProcurementGoodReceiptRepository.generateStockId();
 
         const query = `
-      INSERT INTO \`Procurement Good Receipt Note\` (
-        \`Stock ID\`, \`Style\`, \`Varient Name\`, \`Old Varient\`, 
-        \`Customer Varient\`, \`Base Varient\`,\`Vendor Code\`,  \`Vendor\`, 
-        \`Location\`, \`Department\`, \`Remark 1\`, \`Vendor Varient\`, \`Remark 2\`, \`Created By\`, 
-        \`Std Buying Rate\`, \`Stone Max Wt\`, \`Remark\`, \`Stone Min Wt\`, 
-        \`Karat Color\`, \`Delivery Days\`, \`For Web\`, \`Row Status\`, 
-        \`Verified Status\`, \`Length\`, \`Codegen Sr No\`, \`CATEGORY\`, 
-        \`Sub-Category\`, \`STYLE KARAT\`, \`Varient\`, \`HSN - SAC CODE\`, 
-        \`LINE OF BUSINESS\`, \`BOM\`, \`Operation\`, \`Image Details\`, 
-        \`Formula Details\`
-      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-        const values = [
-            stockId, data.style, data.varientName, data.oldVarient,
-            data.customerVarient, data.baseVarient, data.vendorCode, data.vendor,
-            data.location, data.department, data.remark1, data.vendorVarient, data.remark2, data.createdBy,
-            data.stdBuyingRate, data.stoneMaxWt, data.remark, data.stoneMinWt,
-            data.karatColor, data.deliveryDays, data.forWeb, data.rowStatus,
-            data.verifiedStatus, data.length, data.codegenSrNo, data.category,
-            data.subCategory, data.styleKarat, data.varient, data.hsnSacCode,
-            data.lineOfBusiness, JSON.stringify(data.bom), JSON.stringify(data.operation),
-            JSON.stringify(data.imageDetails), JSON.stringify(data.formulaDetails)
-        ];
+        INSERT INTO \`Procurement Good Receipt Note\` (
+          \`Stock ID\`, \`Style\`, \`Varient Name\`, \`Old Varient\`, 
+          \`Customer Varient\`, \`Base Varient\`, \`Vendor Code\`, \`Vendor\`, 
+          \`Location\`, \`Department\`, \`Remark 1\`, \`Vendor Varient\`, \`Remark 2\`, 
+          \`Created By\`, \`Std Buying Rate\`, \`Stone Max Wt\`, \`Remark\`, 
+          \`Stone Min Wt\`, \`Karat Color\`, \`Delivery Days\`, \`For Web\`, 
+          \`Row Status\`, \`Verified Status\`, \`Length\`, \`Codegen Sr No\`, 
+          \`CATEGORY\`, \`Sub-Category\`, \`STYLE KARAT\`, \`Varient\`, 
+          \`HSN - SAC CODE\`, \`LINE OF BUSINESS\`, \`BOM\`, \`Operation\`, 
+          \`Image Details\`, \`Formula Details\`, \`Pieces\`, \`Weight\`, 
+          \`Net Weight\`, \`Dia Weight\`, \`Dia Pieces\`, \`Location Code\`, 
+          \`Item Group\`, \`Metal Color\`, \`Style Metal Color\`
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+      
+      const values = [
+        stockId, data.style, data.varientName, data.oldVarient,
+        data.customerVarient, data.baseVarient, data.vendorCode, data.vendor,
+        data.location, data.department, data.remark1, data.vendorVarient, data.remark2,
+        data.createdBy, data.stdBuyingRate, data.stoneMaxWt, data.remark, data.stoneMinWt,
+        data.karatColor, data.deliveryDays, data.forWeb, data.rowStatus, data.verifiedStatus,
+        data.length, data.codegenSrNo, data.category, data.subCategory, data.styleKarat,
+        data.varient, data.hsnSacCode, data.lineOfBusiness, JSON.stringify(data.bom),
+        JSON.stringify(data.operation), JSON.stringify(data.imageDetails),
+        JSON.stringify(data.formulaDetails), data.pieces, data.weight, data.netWeight,
+        data.diaWeight, data.diaPieces, data.locationCode, data.itemGroup, data.metalColor,
+        data.styleMetalColor
+      ];
+      
         return new Promise((resolve, reject) => {
             connection.query(query, values, (err, results) => {
                 if (err) {
