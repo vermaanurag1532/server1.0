759bf43a7585588be17def1b63e431c90010ca28
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2025-02-23 23:17:22 +0530

    fix bug

--- Repository/SubContracting/issueWork.repository.js
+++ Repository/SubContracting/issueWork.repository.js
@@ -1,68 +1,97 @@
-import connection from '../../db/connection.js';
+import db from '../../db/connection.js';
 
 const SubContractingIssueWorkRepository = {
+    // Get all records
     getAll: async () => {
-        return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM `Sub Contracting Issue Work`";
-            connection.query(query, (err, results) => {
-                if (err) reject(err);
-                else resolve(results);
-            });
-        });
+        const [rows] = await db.promise().query('SELECT * FROM `Sub Contracting Issue Work`');
+        return rows;
     },
 
+    // Get a record by Stock ID
     getById: async (stockId) => {
-        return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM `Sub Contracting Issue Work` WHERE `stockId` = ?";
-            connection.query(query, [stockId], (err, results) => {
-                if (err) reject(err);
-                else resolve(results[0]);
-            });
-        });
+        const [rows] = await db.promise().query(
+            'SELECT * FROM `Sub Contracting Issue Work` WHERE `Stock ID` = ?',
+            [stockId]
+        );
+        return rows[0];
     },
 
+    // Add a new record
     create: async (data) => {
-        return new Promise((resolve, reject) => {
-            const query = "INSERT INTO `Sub Contracting Issue Work` SET ?";
-            const formattedData = {
-                ...data,
-                bom: JSON.stringify(data.bom),
-                operation: JSON.stringify(data.operation),
-                "imageDetails": JSON.stringify(data["imageDetails"]),
-                "formulaDetails": JSON.stringify(data["formulaDetails"]),
-            };
-            connection.query(query, formattedData, (err, results) => {
-                if (err) reject(err);
-                else resolve(results);
-            });
-        });
+        const query = `
+            INSERT INTO \`Sub Contracting Issue Work\`
+            (\`Stock ID\`, \`Vendor\`, \`Issue Date\`, \`Operation Name\`, \`Style\`, \`Varient Name\`, 
+            \`Old Varient\`, \`Customer Varient\`, \`Base Varient\`, \`Vendor Code\`, \`Location\`, 
+            \`Department\`, \`Remark 1\`, \`Vendor Varient\`, \`Remark 2\`, \`Created By\`, 
+            \`Std Buying Rate\`, \`Stone Max Wt\`, \`Remark\`, \`Stone Min Wt\`, \`Karat Color\`, 
+            \`Delivery Days\`, \`For Web\`, \`Row Status\`, \`Verified Status\`, \`Length\`, 
+            \`Codegen Sr No\`, \`CATEGORY\`, \`Sub-Category\`, \`STYLE KARAT\`, \`Varient\`, 
+            \`HSN - SAC CODE\`, \`LINE OF BUSINESS\`, \`BOM\`, \`Operation\`, \`Image Details\`, 
+            \`Formula Details\`, \`Pieces\`, \`Weight\`, \`Net Weight\`, \`Dia Weight\`, 
+            \`Dia Pieces\`, \`Location Code\`, \`Item Group\`, \`Metal Color\`, \`Style Metal Color\`, 
+            \`Inward Doc\`, \`Last Trans\`, \`isRawMaterial\`)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        `;
+        const values = [
+            data.stockID, data.vendor, data.issueDate, data.operationName, data.style, data.varientName,
+            data.oldVarient, data.customerVarient, data.baseVarient, data.vendorCode, data.location,
+            data.department, data.remark1, data.vendorVarient, data.remark2, data.createdBy,
+            data.stdBuyingRate, data.stoneMaxWt, data.remark, data.stoneMinWt, data.karatColor,
+            data.deliveryDays, data.forWeb, data.rowStatus, data.verifiedStatus, data.length,
+            data.codegenSrNo, data.category, data.subCategory, data.styleKarat, data.varient,
+            data.hsnSacCode, data.lineOfBusiness, JSON.stringify(data.bom), JSON.stringify(data.operation),
+            JSON.stringify(data.imageDetails), JSON.stringify(data.formulaDetails), data.pieces,
+            data.weight, data.netWeight, data.diaWeight, data.diaPieces, data.locationCode,
+            data.itemGroup, data.metalColor, data.styleMetalColor, data.inwardDoc, data.lastTrans,
+            data.isRawMaterial
+        ];
+        const [result] = await db.promise().query(query, values);
+        return result.insertId;
     },
 
+    // Update a record by Stock ID
     update: async (stockId, data) => {
-        return new Promise((resolve, reject) => {
-            const query = "UPDATE `Sub Contracting Issue Work` SET ? WHERE `stockId` = ?";
-            const formattedData = {
-                ...data,
-                bom: JSON.stringify(data.bom),
-                operation: JSON.stringify(data.operation),
-                "imageDetails": JSON.stringify(data["imageDetails"]),
-                "formulaDetails": JSON.stringify(data["formulaDetails"]),
-            };
-            connection.query(query, [formattedData, stockId], (err, results) => {
-                if (err) reject(err);
-                else resolve(results);
-            });
-        });
+        const query = `
+            UPDATE \`Sub Contracting Issue Work\` SET
+            \`Vendor\` = ?, \`Issue Date\` = ?, \`Operation Name\` = ?, \`Style\` = ?, 
+            \`Varient Name\` = ?, \`Old Varient\` = ?, \`Customer Varient\` = ?, 
+            \`Base Varient\` = ?, \`Vendor Code\` = ?, \`Location\` = ?, \`Department\` = ?, 
+            \`Remark 1\` = ?, \`Vendor Varient\` = ?, \`Remark 2\` = ?, \`Created By\` = ?, 
+            \`Std Buying Rate\` = ?, \`Stone Max Wt\` = ?, \`Remark\` = ?, \`Stone Min Wt\` = ?, 
+            \`Karat Color\` = ?, \`Delivery Days\` = ?, \`For Web\` = ?, \`Row Status\` = ?, 
+            \`Verified Status\` = ?, \`Length\` = ?, \`Codegen Sr No\` = ?, \`CATEGORY\` = ?, 
+            \`Sub-Category\` = ?, \`STYLE KARAT\` = ?, \`Varient\` = ?, \`HSN - SAC CODE\` = ?, 
+            \`LINE OF BUSINESS\` = ?, \`BOM\` = ?, \`Operation\` = ?, \`Image Details\` = ?, 
+            \`Formula Details\` = ?, \`Pieces\` = ?, \`Weight\` = ?, \`Net Weight\` = ?, 
+            \`Dia Weight\` = ?, \`Dia Pieces\` = ?, \`Location Code\` = ?, \`Item Group\` = ?, 
+            \`Metal Color\` = ?, \`Style Metal Color\` = ?, \`Inward Doc\` = ?, \`Last Trans\` = ?, 
+            \`isRawMaterial\` = ?
+            WHERE \`Stock ID\` = ?
+        `;
+        const values = [
+            data.vendor, data.issueDate, data.operationName, data.style, data.varientName,
+            data.oldVarient, data.customerVarient, data.baseVarient, data.vendorCode, data.location,
+            data.department, data.remark1, data.vendorVarient, data.remark2, data.createdBy,
+            data.stdBuyingRate, data.stoneMaxWt, data.remark, data.stoneMinWt, data.karatColor,
+            data.deliveryDays, data.forWeb, data.rowStatus, data.verifiedStatus, data.length,
+            data.codegenSrNo, data.category, data.subCategory, data.styleKarat, data.varient,
+            data.hsnSacCode, data.lineOfBusiness, JSON.stringify(data.bom), JSON.stringify(data.operation),
+            JSON.stringify(data.imageDetails), JSON.stringify(data.formulaDetails), data.pieces,
+            data.weight, data.netWeight, data.diaWeight, data.diaPieces, data.locationCode,
+            data.itemGroup, data.metalColor, data.styleMetalColor, data.inwardDoc, data.lastTrans,
+            data.isRawMaterial, stockId
+        ];
+        const [result] = await db.promise().query(query, values);
+        return result.affectedRows;
     },
 
+    // Delete a record by Stock ID
     delete: async (stockId) => {
-        return new Promise((resolve, reject) => {
-            const query = "DELETE FROM `Sub Contracting Issue Work` WHERE `stockId` = ?";
-            connection.query(query, [stockId], (err, results) => {
-                if (err) reject(err);
-                else resolve(results);
-            });
-        });
+        const [result] = await db.promise().query(
+            'DELETE FROM `Sub Contracting Issue Work` WHERE `Stock ID` = ?',
+            [stockId]
+        );
+        return result.affectedRows;
     }
 };
 
